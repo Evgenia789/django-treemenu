0ec5900851403e2936e49771bad43c31da059d2a
Evgenia Pankova <jen.backulina@yandex.ru>
2023-03-01 07:24:35 +0100

    Add docstring to MenuView class, change urlpatterns

--- tree_menu/menu/urls.py
+++ tree_menu/menu/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from menu.views import IndexPageView
+from menu.views import MenuView
 
 app_name = 'menu'
 
 urlpatterns = [
-    path('menu/', IndexPageView.as_view(), name='index')
+    path('', MenuView.as_view(), name='index')
 ]
--- tree_menu/menu/views.py
+++ tree_menu/menu/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
 
 
-class IndexPageView(TemplateView):
+class MenuView(TemplateView):
+    """
+    A view that renders the index page of the menu app.
+    The index page displays the main menu of the application.
+    The menu items are retrieved from the database using the
+    `draw_menu` template tag.
+    """
     template_name = "menu/index.html"
